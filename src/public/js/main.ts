2ca5a032a287c06904c9c5e7584c54607a3f2da4
BenBailey <ben.bailey@ace.edu>
2021-01-29 12:41:20 -0600

    Initial commit

---
+++ src/public/js/main.ts
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import * as BO from '../../BO';
+
+// // tslint:disable no-unused-expression
+new Vue({ // eslint-disable-line no-new
+    computed: {
+    },
+    data() {
+        return {
+            todos: []
+        }
+    },
+    el: "#app",
+    methods: {
+
+    },
+    mounted() {
+        let todos: BO.Todo[] = [
+            { "id": 1, "title": "Integrate Vue.js", "isDone": true },
+            { "id": 2, "title": "Refactor this later", "isDone": false },
+            { "id": 3, "title": "Write an API", "isDone": false },
+            { "id": 5, "title": "Get data from a database", "isDone": false },
+        ];
+        (<BO.Todo[]>this.todos) = todos;
+    }
+});
